7a035ee776507962cf42cc81a5716770d0cb6c32
ypb <obpt@sina.com>
2020-03-09 21:56:23 +0800

    add ignore rules

---
+++ src/test/java/cn/xpbootcamp/legacy_code/service/WalletServiceImplTest.java
@@ -0,0 +1,17 @@
+package cn.xpbootcamp.legacy_code.service;
+
+import cn.xpbootcamp.legacy_code.entity.User;
+import cn.xpbootcamp.legacy_code.repository.UserRepository;
+import cn.xpbootcamp.legacy_code.repository.UserRepositoryImpl;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+public class WalletServiceImplTest {
+
+    @Test
+    public void should(){
+
+    }
+}
