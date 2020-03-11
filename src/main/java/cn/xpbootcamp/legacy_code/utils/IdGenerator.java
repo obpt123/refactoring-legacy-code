9f866856d74eac0648736081d8a756e39e62fd71
ypb <obpt@sina.com>
2020-03-11 12:49:05 +0800

    remove IdGenerator util

--- src/main/java/cn/xpbootcamp/legacy_code/utils/IdGenerator.java
+++
@@ -1,9 +0,0 @@
-package cn.xpbootcamp.legacy_code.utils;
-
-import java.util.UUID;
-
-public class IdGenerator {
-    public static String generateTransactionId() {
-        return UUID.randomUUID().toString();
-    }
-}
--- src/test/java/cn/xpbootcamp/legacy_code/service/IdGeneratorImplTest.java
+++ src/test/java/cn/xpbootcamp/legacy_code/service/IdGeneratorImplTest.java
@@ -1,15 +1,16 @@
-package cn.xpbootcamp.legacy_code.utils;
+package cn.xpbootcamp.legacy_code.service;
 
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-public class IdGeneratorTest {
+public class IdGeneratorImplTest {
     @Test
     public void shouldReturnUniqueStringWhenGenerateTransactionId() {
+        IdGenerator idGenerator=new IdGeneratorImpl();
         int loopCount = 10000;
-        long totalIds = IntStream.range(0, loopCount).boxed().map((p) -> IdGenerator.generateTransactionId()).distinct()
+        long totalIds = IntStream.range(0, loopCount).boxed().map((p) -> idGenerator.generateTransactionId()).distinct()
                 .count();
         assertEquals(loopCount, totalIds);
     }
