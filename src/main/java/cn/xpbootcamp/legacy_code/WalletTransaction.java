479eb08edeeaa3192582b8c4d9d5096ae7475a55
ypb <obpt@sina.com>
2020-03-11 15:36:13 +0800

    remove class level field walletTransactionId

--- src/main/java/cn/xpbootcamp/legacy_code/WalletTransaction.java
+++ src/main/java/cn/xpbootcamp/legacy_code/WalletTransaction.java
@@ -18,7 +18,6 @@ public class WalletTransaction {
 
     private Order order;
     private STATUS status;
-    private String walletTransactionId;
 
     WalletService walletService = new WalletServiceImpl();
     DistributedLock distributedLock = new RedisDistributedLockImpl();
@@ -64,13 +63,9 @@ public class WalletTransaction {
                 status = STATUS.EXPIRED;
                 return;
             }
-            walletTransactionId = walletService.moveMoney(id, order.getBuyerId(), order.getSellerId(),
+            String moveMoneyResult = walletService.moveMoney(id, order.getBuyerId(), order.getSellerId(),
                     order.getAmount());
-            if (walletTransactionId != null) {
-                this.status = STATUS.EXECUTED;
-            } else {
-                this.status = STATUS.FAILED;
-            }
+            status = moveMoneyResult != null ? STATUS.EXECUTED : STATUS.FAILED;
         });
         return STATUS.EXECUTED == status;
     }
